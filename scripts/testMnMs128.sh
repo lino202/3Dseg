04da69ddd32855d81c548c630585efe30e11b48f
lino202 <maxi.rosales202@gmail.com>
2023-03-17 23:28:42 +0100

    Added script test with ph postpro

--- scripts/testMnMs128.sh
+++ scripts/testMnMs128.sh
@@ -4,7 +4,7 @@
 python /home/maxi/Segmentation/Code/3Dseg/test.py \
 --root_path        /home/maxi/Segmentation/Data/seg3D_MnMs/128x128_ED \
 --results_dir      /home/maxi/Segmentation/Results/seg3D_MnMs/128x128_ED \
---name             init \
+--name             50_50_CE_128_4_1_test1 \
 --phase            test \
 --input_nc         1 \
 --output_nc        4 \
@@ -13,8 +13,11 @@ python /home/maxi/Segmentation/Code/3Dseg/test.py \
 --load_size_h      128 \
 --load_size_w      128 \
 --load_size_d      128 \
+--ph               \
 --phParallel       \
+--priorName        prior_CINE_MnMs \
+--phThres          0.5 \
 --load_filename    latest_net \
---res_excel        /home/maxi/Segmentation/Results/seg3D_MnMs/128x128/results.xlsx \
---res_params_name  results_baseline \
+--res_excel        /home/maxi/Segmentation/Results/seg3D_MnMs/128x128_ED/results.xlsx \
+--res_params_name  results_baseline_ph_crop \
 --res_excel_indexs gdsc_bg gdsc_lv gdsc_myo gdsc_rv hd_bg hd_lv hd_myo hd_rv be ts
