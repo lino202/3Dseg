861db11c522e9de9704431a0d2fcf37d9c3040c8
lino202 <maxi.rosales202@gmail.com>
2023-03-21 15:55:37 +0100

    Added train emide script

---
+++ scripts/trainEmidec128.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python F:/Segmentation/Code/3Dseg/train.py \
+--root_path       /home/maxi/Segmentation/Data/seg3D_emidec/128x128/roi \
+--results_dir     /home/maxi/Segmentation/Results/seg3D_emidec/roi \
+--name            50_50_CE_128_4_1_test1 \
+--phase           train \
+--input_nc        1 \
+--output_nc       1 \
+--nfl             64 \
+--num_downs       7 \
+--n_epochs        50 \
+--n_epochs_decay  50 \
+--loss            Dice \
+--load_size_h     128 \
+--load_size_w     128 \
+--load_size_d     128 \
+--save_epoch_freq 5 \
+--batch_size      1 \
