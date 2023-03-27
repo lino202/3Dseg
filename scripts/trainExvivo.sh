66828e107d7f6b6ad17f6549dc38e135710190da
lino202 <maxi.rosales202@gmail.com>
2023-03-27 17:57:07 +0200

    Script for exvivo trainning tunned

--- scripts/trainExvivo.sh
+++ scripts/trainExvivo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python /home/maxi/Segmentation/Code/3Dseg/train.py \
---root_path       /home/maxi/Segmentation/Data/seg3D_exvivo \
---results_dir     /home/maxi/Segmentation/Results/seg3D_exvivo \
+--root_path       /home/maxi/Segmentation/Data/seg3D_exvivo/noph_checked \
+--results_dir     /home/maxi/Segmentation/Results/seg3D_exvivo/noph_checked \
 --name            50_50_CE_128_2_1 \
 --phase           train \
 --input_nc        1 \
