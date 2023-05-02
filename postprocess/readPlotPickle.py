5493ae2ab203df66e20518dbd9598552c7c5e02f
lino202 <maxi.rosales202@gmail.com>
2023-05-02 17:32:36 +0200

    Added multi approach plot - postproccessing

--- postprocess/readPlotPickle.py
+++ postprocess/readPlotPickle.py
@@ -8,26 +8,29 @@ import os
 
 sns.set(style='whitegrid')
 
-def map2DF(dataBaseline, dataPH, label):
-    n = dataBaseline[list(dataBaseline.keys())[0]].shape[0]
+def map2DF(data, label):
+    # This work as long as all keys have array associated with the same length
+    tmpkey = list(data.keys())[0]
+    n = data[tmpkey][list(data[tmpkey].keys())[0]].shape[0]
     mydict = {}
     mydict['approach'] = []
     mydict[label]     = np.array([])
     mydict['class']    = []
-    for key in dataBaseline.keys():
-        if label.lower() in key and not 'bg' in key:
-            tmp = np.concatenate((dataBaseline[key], dataPH[key]))
-            mydict[label] = np.concatenate((mydict[label], tmp)) if mydict[label].size else tmp
-            tmp = [key.split('_')[-1]] * n * 2
-            mydict['class'] = [*mydict['class'], *tmp]
-            tmp = [*['baseline'] * n, *['ph'] * n]
-            mydict['approach'] = [*mydict['approach'], *tmp]
+    for approach in data.keys():
+        for key in data[approach].keys():
+            if label.lower() in key and not 'bg' in key:
+                # tmp = np.concatenate((dataBaseline[key], dataPH[key]))
+                mydict[label] = np.concatenate((mydict[label], data[approach][key])) if mydict[label].size else data[approach][key]
+                tmp = [key.split('_')[-1]] * n
+                mydict['class'] = [*mydict['class'], *tmp]
+                tmp = [approach] * n
+                mydict['approach'] = [*mydict['approach'], *tmp]
     return mydict
 
-def plot(data, label, resPath='0'):
+def plot(data, label, resPath):
     plt.figure()
     sns.boxplot(data=data, x='class', y=label, hue='approach')
-    if resPath != '0':
+    if resPath:
         plt.savefig(os.path.join(resPath, "{}.png".format(label)))
     else:
         plt.show()
@@ -35,27 +38,29 @@ def plot(data, label, resPath='0'):
 
 def main():
     parser = argparse.ArgumentParser(description="Options")
-    parser.add_argument('--filePathBaseline', required=True, type=str)
-    parser.add_argument('--filePathPH',       required=True, type=str)
-    parser.add_argument('--resPath',          required=True, type=str)
+    parser.add_argument('--filePaths',  nargs='+', required=True, type=str)
+    parser.add_argument('--classes',    nargs='+', required=True, type=str)
+    parser.add_argument('--resPath', type=str)
     args = parser.parse_args()
     
-    dataBaseline = pd.read_pickle(args.filePathBaseline)
-    dataPH = pd.read_pickle(args.filePathPH)
+    data = {}
+    for i, key in enumerate(args.classes):
+        data[key] = pd.read_pickle(args.filePaths[i])
+    
     # print((dataPH["hd_lv"]==np.inf).nonzero())
     # print((dataPH["hd_myo"]==np.inf).nonzero())
     # print((dataPH["hd_rv"]==np.inf).nonzero())
     
     #REMAP into DF--------------------------------------------
-    gdsc = map2DF(dataBaseline, dataPH, 'gDSC')
-    hd = map2DF(dataBaseline, dataPH, 'HD')
-    be = map2DF(dataBaseline, dataPH, 'BE')
-    ts = map2DF(dataBaseline, dataPH, 'TS')
+    gdsc = map2DF(data, 'gDSC')
+    hd   = map2DF(data, 'HD')
+    be   = map2DF(data, 'BE')
+    ts   = map2DF(data, 'TS')
             
     gdsc = pd.DataFrame(gdsc)
-    hd = pd.DataFrame(hd)
-    be = pd.DataFrame(be)
-    ts = pd.DataFrame(ts)
+    hd   = pd.DataFrame(hd)
+    be   = pd.DataFrame(be)
+    ts   = pd.DataFrame(ts)
     
     #Save or plot ----------------------------------
     plot(gdsc, 'gDSC', args.resPath)
