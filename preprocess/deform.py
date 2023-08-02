c75b369f5e5a5efccbc1c888d9c5d63f578eee5b
lino202 <maxi.rosales202@gmail.com>
2023-08-02 18:24:26 +0200

    Added symlinks CV for deform and adjust elastic deformation max disp to be an argument

--- preprocess/deform.py
+++ preprocess/deform.py
@@ -23,6 +23,7 @@ def main():
     parser.add_argument('--rootPath',    type=str)
     parser.add_argument('--resPath',     type=str)
     parser.add_argument('--plotPath',     type=str)
+    parser.add_argument('--maxDisp',     type=float)
     args = parser.parse_args()
     
     if not os.path.exists(args.resPath): pathlib.Path(args.resPath).mkdir(parents=True, exist_ok=True)
@@ -39,8 +40,8 @@ def main():
     )
     
     transElastic = tio.RandomElasticDeformation(
-        num_control_points=(7,7,7), 
-        max_displacement=(12.,12.,0.), 
+        num_control_points=(5,5,5), 
+        max_displacement=(args.maxDisp,args.maxDisp,0.), 
         locked_borders=2,
         image_interpolation='nearest'
     )
@@ -58,13 +59,15 @@ def main():
         mskTrans = tio.LabelMap(tensor=mskTrans, affine=msk.affine)
       
         f, ax = plt.subplots(2,3)
-        s = int(np.round(msk.shape[-1]/2))
-        ax[0,0].imshow(msk.data.numpy()[0,:,:,s], vmin=0, vmax=3, interpolation='none')
-        ax[0,1].imshow(msk.data.numpy()[0,:,s,:], vmin=0, vmax=3, interpolation='none')
-        ax[0,2].imshow(msk.data.numpy()[0,s,:,:], vmin=0, vmax=3, interpolation='none')
-        ax[1,0].imshow(mskTrans.data.numpy()[0,:,:,s], vmin=0, vmax=3, interpolation='none')
-        ax[1,1].imshow(mskTrans.data.numpy()[0,:,s,:], vmin=0, vmax=3, interpolation='none')
-        ax[1,2].imshow(mskTrans.data.numpy()[0,s,:,:], vmin=0, vmax=3, interpolation='none')
+        s2 = int(np.round(msk.shape[-1]/2))
+        s1 = int(np.round(msk.shape[-2]/2))
+        s0 = int(np.round(msk.shape[-3]/2))
+        ax[0,0].imshow(msk.data.numpy()[0,:,:,s2], vmin=0, vmax=3, interpolation='none')
+        ax[0,1].imshow(msk.data.numpy()[0,:,s1,:], vmin=0, vmax=3, interpolation='none')
+        ax[0,2].imshow(msk.data.numpy()[0,s0,:,:], vmin=0, vmax=3, interpolation='none')
+        ax[1,0].imshow(mskTrans.data.numpy()[0,:,:,s2], vmin=0, vmax=3, interpolation='none')
+        ax[1,1].imshow(mskTrans.data.numpy()[0,:,s1,:], vmin=0, vmax=3, interpolation='none')
+        ax[1,2].imshow(mskTrans.data.numpy()[0,s0,:,:], vmin=0, vmax=3, interpolation='none')
         plt.savefig(os.path.join(args.plotPath, "{}.png".format(sample))) if args.plotPath else plt.show()
         plt.close()
         
