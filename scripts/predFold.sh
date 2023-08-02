b9e30eb4989815fefe50baa73a0b8cfd63cf0c45
lino202 <maxi.rosales202@gmail.com>
2023-08-02 15:35:01 +0200

    Modified for predicting all cases

--- scripts/predFold.sh
+++ scripts/predFold.sh
@@ -1,9 +1,94 @@
 
 
 python /home/maxi/Segmentation/Code/3Dseg/pred.py \
---root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_0/val \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_1/val \
 --results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
---name                 fold_0 \
+--name                 fold_1 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            2 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           128 128 128 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_EXVIVO
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_2/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
+--name                 fold_2 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            2 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           128 128 128 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_EXVIVO
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_3/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
+--name                 fold_3 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            2 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           128 128 128 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_EXVIVO
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_4/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
+--name                 fold_4 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            2 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           128 128 128 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_EXVIVO
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_5/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
+--name                 fold_5 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            2 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           128 128 128 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_EXVIVO
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/Exvivo/noph_checked_onlyStandfor/CV/fold_6/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/Exvivo \
+--name                 fold_6 \
 --phase                pred \
 --input_nc             1 \
 --output_nc            2 \
@@ -19,10 +104,68 @@ python /home/maxi/Segmentation/Code/3Dseg/pred.py \
 
 
 
+
+
+
+
+
+
+
 python /home/maxi/Segmentation/Code/3Dseg/pred.py \
---root_path            /home/maxi/Segmentation/Data_paper2/MnM/CV/fold_0/val \
+--root_path            /home/maxi/Segmentation/Data_paper2/MnM/CV/fold_1/val \
 --results_dir          /home/maxi/Segmentation/Results_paper2/MnM \
---name                 fold_0 \
+--name                 fold_1 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           160 160 20 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_CINE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/MnM/CV/fold_2/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/MnM \
+--name                 fold_2 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           160 160 20 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_CINE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/MnM/CV/fold_3/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/MnM \
+--name                 fold_3 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           160 160 20 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_CINE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/MnM/CV/fold_4/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/MnM \
+--name                 fold_4 \
 --phase                pred \
 --input_nc             1 \
 --output_nc            4 \
@@ -38,10 +181,68 @@ python /home/maxi/Segmentation/Code/3Dseg/pred.py \
 
 
 
+
+
+
+
+
+
+
 python /home/maxi/Segmentation/Code/3Dseg/pred.py \
---root_path            /home/maxi/Segmentation/Data_paper2/LGE/96x96x24/CV_mi/fold_0/val \
+--root_path            /home/maxi/Segmentation/Data_paper2/LGE/96x96x24/CV_mi/fold_1/val \
 --results_dir          /home/maxi/Segmentation/Results_paper2/LGE/96x96x24_mi \
---name                 fold_0 \
+--name                 fold_1 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           96 96 24 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_LGE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/LGE/96x96x24/CV_mi/fold_2/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/LGE/96x96x24_mi \
+--name                 fold_2 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           96 96 24 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_LGE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/LGE/96x96x24/CV_mi/fold_3/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/LGE/96x96x24_mi \
+--name                 fold_3 \
+--phase                pred \
+--input_nc             1 \
+--output_nc            4 \
+--nfl                  64 \
+--num_downs            5 \
+--patch_size           96 96 24 \
+--load_filename        latest_net \
+--norm                 instance \
+--ph                   \
+--phThres              0.5 \
+--phConstruction       N \
+--priorName            PRIOR_LGE
+
+python /home/maxi/Segmentation/Code/3Dseg/pred.py \
+--root_path            /home/maxi/Segmentation/Data_paper2/LGE/96x96x24/CV_mi/fold_4/val \
+--results_dir          /home/maxi/Segmentation/Results_paper2/LGE/96x96x24_mi \
+--name                 fold_4 \
 --phase                pred \
 --input_nc             1 \
 --output_nc            4 \
